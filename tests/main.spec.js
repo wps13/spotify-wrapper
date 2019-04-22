63215fb1e7eee343924da0798a79b723531399f2
wps13 <willanepaiva@hotmail.com>
2019-04-22 18:13:10 -0300

    #02 - create smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,32 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import {
+  search,
+  searchArtists,
+  searchTracks,
+  searchPlaylists
+} from "../src/main";
 
-describe('Main', () => {
+describe("Spotify wrapper", () => {
+  describe("smoke tests", () => {
+    //search
+    // searchArtists
+    // searchTracks
+    // searchPlaylists
 
+    it("should exist the search method", () => {
+      expect(search).to.exist;
+    });
+
+    it("should exist the searchArtists method", () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it("should exist the searchTracks method", () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it("should exist the searchPlaylists method", () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 });
