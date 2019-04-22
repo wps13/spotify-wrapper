a109c7310a03b488edebafc85d9c5e9619d716de
wps13 <willanepaiva@hotmail.com>
2019-04-22 18:53:44 -0300

    # 05 - Add tests for generic search

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,4 +1,14 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
+
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+import sinonStubPromise from "sinon-stub-promise";
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require("node-fetch");
+
 import {
   search,
   searchArtists,
@@ -27,6 +37,53 @@ describe("Spotify wrapper", () => {
 
     it("should exist the searchPlaylists method", () => {
       expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe("Generic search", () => {
+    let fetchedStub;
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, "fetch");
+    });
+
+    afterEach(() => {
+      fetchedStub.restore();
+    });
+
+    it("should call fetch function", () => {
+      const artists = search();
+      expect(fetchedStub).to.have.been.calledOnce;
+      fetchedStub.restore();
+    });
+
+    it("should receive correct url to fetch", () => {
+      context("passing one type", () => {
+        const artists = search("Blackpink", "artist");
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=Blackpink&type=artist"
+        );
+
+        const artists2 = search("Jennie", "artist");
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=Jennie&type=artist"
+        );
+
+        const albums = search("kill%20this%20love", "album");
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=kill%20this%20love&type=album"
+        );
+        const albums2 = search("solo", "album");
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=solo&type=album"
+        );
+      });
+
+      context("passing more than one type", () => {
+        const artistsAndAlbums = search("Blackpink", ["artist", "album"]);
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=Blackpink&type=artist,album"
+        );
+      });
     });
   });
 });
